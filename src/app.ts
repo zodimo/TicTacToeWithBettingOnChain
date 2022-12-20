0c15195691a8cec34933a76c728017de150bb902
Jaco Labuschagne <zodimo@gmail.com>
2022-12-20 11:46:26 +0200

    extract cardanocli instance

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,3 @@
-import CardanocliJs from "cardanocli-js";
-import os from "os";
-import path from "path";
-
-const dir = path.join(os.homedir(), "CardanoProjects", "testnet");
-const shelleyPath = path.join(
-  os.homedir(),
-  "CardanoProjects",
-  "testnet",
-  "config",
-  "preview",
-  "shelley-genesis.json"
-);
-
-const cardanocliJs = new CardanocliJs({
-  network: "testnet-magic 2",
-  era: "babbage",
-  dir: dir,
-  shelleyGenesisPath: shelleyPath,
-  socketPath: path.join(
-    os.homedir(),
-    "CardanoProjects",
-    "testnet",
-    "node.socket"
-  ),
-});
+import cardanocliJs from "./previewCardanoCliJs.js";
 
 console.log(cardanocliJs.queryTip());
---
+++ src/previewCardanoCliJs.ts
@@ -0,0 +1,28 @@
+import CardanocliJs from "cardanocli-js";
+import os from "os";
+import path from "path";
+
+const dir = path.join(os.homedir(), "CardanoProjects", "testnet");
+const shelleyPath = path.join(
+  os.homedir(),
+  "CardanoProjects",
+  "testnet",
+  "config",
+  "preview",
+  "shelley-genesis.json"
+);
+
+const cardanocliJs = new CardanocliJs({
+  network: "testnet-magic 2",
+  era: "babbage",
+  dir: dir,
+  shelleyGenesisPath: shelleyPath,
+  socketPath: path.join(
+    os.homedir(),
+    "CardanoProjects",
+    "testnet",
+    "node.socket"
+  ),
+});
+
+export default cardanocliJs;
