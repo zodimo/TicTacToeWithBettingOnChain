19bd91f97f4391ccc6a9f823b80fea0b3e743cef
Jaco Labuschagne <zodimo@gmail.com>
2022-12-25 17:23:50 +0200

    cleanup

--- src/utxo-stack-at-script-address.ts
+++
@@ -1,4 +0,0 @@
-import { cardanoCli } from "./new-cardano-cli.js";
-import { getScriptAddress } from "./smart-contract.js";
-const scriptAddress = getScriptAddress();
-console.log(cardanoCli.getUtxoStackFor(scriptAddress));
