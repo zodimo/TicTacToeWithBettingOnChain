b31d09ada31c4061886d70569c6725ba35fcce13
Jaco Labuschagne <zodimo@gmail.com>
2022-12-29 08:38:13 +0200

    addess key-hash

--- src/cardano-cli/command/address/key-hash.ts
+++ src/cardano-cli/command/address/key-hash.ts
@@ -1,3 +1,7 @@
+import { CommandParameter } from "../command-parameter.js";
+import { Command } from "../command.js";
+import { OutFile } from "../shared/out-file.js";
+
 /*
 Usage: cardano-cli address key-hash 
             ( --payment-verification-key STRING
@@ -5,15 +9,55 @@ Usage: cardano-cli address key-hash
             )
             [--out-file FILE]
 */
+class PaymentVerificationKey extends CommandParameter {
+  private constructor(private paramKey: string, private paramValue: string) {
+    super();
+  }
 
-import { Command } from "../command";
+  static value(value: string): PaymentVerificationKey {
+    //--payment-verification-key STRING
+    const param = "normal-key";
+    return new PaymentVerificationKey(param, value);
+  }
+  static extended(value: string): PaymentVerificationKey {
+    // --payment-verification-key-file FILE
+    const param = "payment-verification-key-file";
+    return new PaymentVerificationKey(param, value);
+  }
+
+  asParameter(): string {
+    return `--${this.paramKey} ${this.paramValue}`;
+  }
+}
 
 export class KeyHash extends Command {
+  private paymentVerificationKey?: PaymentVerificationKey;
+  private outFile?: OutFile;
+
   constructor(private commandPrefix: string) {
     super();
   }
 
+  withPaymentVerificationKey(
+    paymentVerificationKey: PaymentVerificationKey
+  ): KeyHash {
+    this.paymentVerificationKey = paymentVerificationKey;
+    return this;
+  }
+
+  withOutFile(outFile: OutFile): KeyHash {
+    this.outFile = outFile;
+    return this;
+  }
+
   getCommand(): string {
-    throw new Error("Method not implemented.");
+    let ouput: string[] = [this.commandPrefix, "key-hash"];
+    if (this.paymentVerificationKey) {
+      ouput.push(this.paymentVerificationKey.asParameter());
+    }
+    if (this.outFile) {
+      ouput.push(this.outFile.asParameter());
+    }
+    return ouput.join(" ");
   }
 }
