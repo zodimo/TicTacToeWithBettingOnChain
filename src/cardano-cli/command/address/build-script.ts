1ade6baf6304c418b8a615b29919cfbfcf66610b
Jaco Labuschagne <zodimo@gmail.com>
2022-12-29 10:20:43 +0200

    implement build-script and info

--- src/cardano-cli/command/address/build-script.ts
+++ src/cardano-cli/command/address/build-script.ts
@@ -1,17 +1,48 @@
+import { Command } from "../command.js";
+import { Network } from "../network.js";
+import { OutFile } from "../shared/out-file.js";
+
 /*
 Usage: cardano-cli address build-script --script-file FILE
             (--mainnet | --testnet-magic NATURAL)
             [--out-file FILE]
 */
 
-import { Command } from "../command";
-
 export class BuildScript extends Command {
+  private scriptFile?: string;
+  private network?: Network;
+  private outFile?: OutFile;
   constructor(private commandPrefix: string) {
     super();
   }
-  
+
+  withScriptFile(scriptFile: string): BuildScript {
+    this.scriptFile = scriptFile;
+    return this;
+  }
+  withNetwork(network: Network): BuildScript {
+    this.network = network;
+    return this;
+  }
+
+  withOutFile(outFile: OutFile): BuildScript {
+    this.outFile = outFile;
+    return this;
+  }
+
   getCommand(): string {
-    throw new Error("Method not implemented.");
+    let ouput: string[] = [this.commandPrefix, "build-script"];
+
+    if (this.scriptFile) {
+      ouput.push(`--script-file ${this.scriptFile}`);
+    }
+
+    if (this.network) {
+      ouput.push(this.network.asParameter());
+    }
+    if (this.outFile) {
+      ouput.push(this.outFile.asParameter());
+    }
+    return ouput.join(" ");
   }
 }
--- src/cardano-cli/command/address/info.ts
+++ src/cardano-cli/command/address/info.ts
@@ -1,15 +1,34 @@
+import { Command } from "../command.js";
+import { OutFile } from "../shared/out-file.js";
+
 /*
 Usage: cardano-cli address info --address ADDRESS [--out-file FILE]
 */
 
-import { Command } from "../command";
-
 export class Info extends Command {
+  private address?: string;
+  private outFile?: OutFile;
   constructor(private commandPrefix: string) {
     super();
   }
-  
+  withAddress(address: string): Info {
+    this.address = address;
+    return this;
+  }
+  withOutFile(outFile: OutFile): Info {
+    this.outFile = outFile;
+    return this;
+  }
   getCommand(): string {
-    throw new Error("Method not implemented.");
+    let ouput: string[] = [this.commandPrefix, "info"];
+
+    if (this.address) {
+      ouput.push(`--address ${this.address}`);
+    }
+
+    if (this.outFile) {
+      ouput.push(this.outFile.asParameter());
+    }
+    return ouput.join(" ");
   }
 }
