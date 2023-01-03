fce263bbc5a02f4e928c06489492856fe1a13022
Jaco Labuschagne <zodimo@gmail.com>
2023-01-03 19:57:28 +0200

    convert to commands

--- src/game-play/offchain/game3-cancel-initiated-game.ts
+++ src/game-play/offchain/game3-cancel-initiated-game.ts
@@ -1,17 +1,13 @@
 import { GameState, GameStateFactory, StartGameParams } from "../../app/game-data.js";
-import { Game, GamePayOut } from "../../app/game.js";
+import { CancelInitiatedGameCommand, Game, GamePayOut, StartGameCommand } from "../../app/game.js";
 import { runCommand } from "../../cardano-cli/run-command.js";
 import { ScriptDataJsonSchema } from "../../cardano-cli/script-data.js";
 
 // TX 1
 const playerOneAddress = "Player1Address";
 const startGameParams = new StartGameParams(playerOneAddress, 50, 1);
-
-const tx1: (startGameParams: StartGameParams) => GameState = (startGameParams) => {
-  return Game.startGame(startGameParams).gameState;
-};
-
-const tx1GameState = tx1(startGameParams);
+const tx1Command = new StartGameCommand(startGameParams);
+const tx1GameState = Game.handleActionCommand(tx1Command);
 const tx1GameStateAsScriptData = tx1GameState.toScriptData();
 console.log("##########################");
 console.log(`tx1 [${tx1GameState.constructor.name}]: ${JSON.stringify(tx1GameState, null, 2)}`);
@@ -27,8 +23,7 @@ runCommand(`sleep 5`);
 
 // Cancel game
 
-const tx2: (gameState: GameState) => GamePayOut = (gamestate) => {
-  return Game.loadGame(gamestate).cancelInitiatedGame();
-};
 const tx1GameStateFromScriptData = new GameStateFactory().fromScriptData(tx1GameStateAsScriptData);
-console.log(tx2(tx1GameStateFromScriptData));
+const tx2Command = new CancelInitiatedGameCommand(tx1GameStateFromScriptData);
+const payout = Game.handleEndGameActionCommand(tx2Command);
+console.log(payout);
--- src/game-play/offchain/game4-cancel-inprogress-game.ts
+++ src/game-play/offchain/game4-cancel-inprogress-game.ts
@@ -1,17 +1,13 @@
 import { GameState, GameStateFactory, JoinGameParams, StartGameParams } from "../../app/game-data.js";
-import { Game, GamePayOut } from "../../app/game.js";
+import { CancelInProgressGameCommand, Game, GamePayOut, JoinGameCommand, StartGameCommand } from "../../app/game.js";
 import { runCommand } from "../../cardano-cli/run-command.js";
 import { ScriptDataJsonSchema } from "../../cardano-cli/script-data.js";
 
 // TX 1
 const playerOneAddress = "Player1Address";
 const startGameParams = new StartGameParams(playerOneAddress, 50, 1);
-
-const tx1: (startGameParams: StartGameParams) => GameState = (startGameParams) => {
-  return Game.startGame(startGameParams).gameState;
-};
-
-const tx1GameState = tx1(startGameParams);
+const tx1Command = new StartGameCommand(startGameParams);
+const tx1GameState = Game.handleActionCommand(tx1Command);
 const tx1GameStateAsScriptData = tx1GameState.toScriptData();
 console.log("##########################");
 console.log(`tx1 [${tx1GameState.constructor.name}]: ${JSON.stringify(tx1GameState, null, 2)}`);
@@ -26,12 +22,9 @@ console.log(
 // TX 2
 const playerTwoAddress = "Player2Address";
 const joinGameParams = new JoinGameParams(playerTwoAddress);
-
-const tx2: (gameState: GameState, joinGameParams: JoinGameParams) => GameState = (gamestate, joinGameParams) => {
-  return Game.loadGame(gamestate).joinGame(joinGameParams).gameState;
-};
 const tx1GameStateFromScriptData = new GameStateFactory().fromScriptData(tx1GameStateAsScriptData);
-const tx2GameState = tx2(tx1GameStateFromScriptData, joinGameParams);
+const tx2Command = new JoinGameCommand(tx1GameStateFromScriptData, joinGameParams);
+const tx2GameState = Game.handleActionCommand(tx2Command);
 const tx2GameStateAsScriptData = tx2GameState.toScriptData();
 console.log("##########################");
 console.log(`tx2 [${tx2GameState.constructor.name}]: ${JSON.stringify(tx2GameState, null, 2)}`);
@@ -46,8 +39,7 @@ console.log(
 console.log("sleeping for 5 seconds");
 runCommand(`sleep 5`);
 
-const tx3: (gameState: GameState) => GamePayOut = (gamestate) => {
-  return Game.loadGame(gamestate).cancelInProgressGame();
-};
 const tx2GameStateFromScriptData = new GameStateFactory().fromScriptData(tx2GameStateAsScriptData);
-console.log(tx3(tx2GameStateFromScriptData));
+const tx3Command=new CancelInProgressGameCommand(tx2GameStateFromScriptData);
+const payout=Game.handleEndGameActionCommand(tx3Command);
+console.log(payout);
