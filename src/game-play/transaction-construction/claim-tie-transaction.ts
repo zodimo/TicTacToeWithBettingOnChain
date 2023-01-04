9ac2a1bcea8185adcdda45bf9862e5b413e1a74a
Jaco Labuschagne <zodimo@gmail.com>
2023-01-04 11:04:59 +0200

    add more usecase transactions

---
+++ src/game-play/transaction-construction/claim-tie-transaction.ts
@@ -0,0 +1,102 @@
+import assert from "assert";
+import { Game, GameIsTiePayout, Payout } from "../../app/game.js";
+import { ClaimTieCommand, GameStateFactory } from "../../app/game-data.js";
+import { RequiredSigner } from "../../cardano-cli/command/transaction/build/required-signer.js";
+import { TxOut, TxOutParameter } from "../../cardano-cli/command/transaction/build/tx-out.js";
+import { SigningKeyFile } from "../../cardano-cli/command/transaction/sign/signing-key-file.js";
+import { TxToSign } from "../../cardano-cli/command/transaction/sign/tx-to-sign.js";
+import { fromJson } from "../../cardano-cli/script-data.js";
+import { TransactionBuildOptions } from "../../cardano-cli/transaction-build-options.js";
+import { TransactionSignOptions } from "../../cardano-cli/transaction/sign-options.js";
+import { TransactionSubmitOptions } from "../../cardano-cli/transaction/submit-options.js";
+import { UtxoId } from "../../cardano-cli/utxo-id.js";
+import { Wallet } from "../../cardano-cli/wallet.js";
+import { cardanoCli } from "../../previewCardanoCliJs.js";
+import {
+  getUtxoFromScriptAddress,
+  utxoAsTransactionBuildInput,
+  utxoStackAsInput,
+  utxoStackForWallet,
+} from "./utils.js";
+import { writeCommandParamsAsRedeemerToFile } from "./write-script-data-to-file.js";
+
+export const sendClaimTieCommandToScriptTransaction: (
+  playerWallet: Wallet,
+  scriptAddress: string,
+  scriptFile: string,
+  scriptUtxoIdWithGameState: UtxoId,
+  playerWallets: Wallet[]
+) => UtxoId[] = (playerWallet, scriptAddress, scriptFile, gameStateUtxoId, playerWallets) => {
+  console.log("###########################");
+  console.log("######## CLAIM TIE ########");
+  console.log("###########################");
+
+  assert.equal(playerWallets.length == 2, true, "Expected both player wallets");
+
+  const utxoFromStack = getUtxoFromScriptAddress(gameStateUtxoId, scriptAddress);
+
+  const scriptData = fromJson(JSON.stringify(utxoFromStack.inlineDatum));
+  const gameStateFromScriptData = new GameStateFactory().fromScriptData(scriptData);
+  const playerPubKeyHash = cardanoCli.pubKeyHashFromVerificationKeyFile(playerWallet.keys.payment.verificationKeyFile);
+
+  const command = new ClaimTieCommand(gameStateFromScriptData);
+  //us payout to create outputs to transaction.
+  const gamePayout = Game.handleEndGameActionCommand(command);
+  const commandRedeemerile = writeCommandParamsAsRedeemerToFile(command);
+
+  // const outputValueInLovelace = utxoFromStack.value.lovelace;
+
+  const payOutToWallet: (payout: Payout, playerWallets: Wallet[]) => Wallet = (payout, wallets) => {
+    const maybeIdentifiedWallet = wallets.find((wallet) => {
+      return cardanoCli.pubKeyHashFromVerificationKeyFile(wallet.keys.payment.verificationKeyFile) == payout.pubKeyHash;
+    });
+    assert.equal(!!maybeIdentifiedWallet, true, "No Wallet pub key hash matches!");
+    return maybeIdentifiedWallet as Wallet;
+  };
+
+  const gamePayoutAsTxOutParameters: (payout: Payout, wallet: Wallet) => TxOutParameter = (payout, wallet) => {
+    // @TODO improve this very naive approach
+    // determine who the pubKeyHash Belongs to...
+    // which value to use ? value in payout or on utxo ?
+
+    const winningWalletPubKeyHash = cardanoCli.pubKeyHashFromVerificationKeyFile(
+      wallet.keys.payment.verificationKeyFile
+    );
+    assert.equal(winningWalletPubKeyHash == payout.pubKeyHash, true, "Expects pub key hash to match!");
+
+    const payoutInLoveLace = cardanoCli.toLovelace(payout.amountInAda);
+
+    return new TxOutParameter(new TxOut(wallet.paymentAddr, payoutInLoveLace));
+  };
+
+  const payouts = (gamePayout as GameIsTiePayout).payouts;
+  const wallet1 = payOutToWallet(payouts[0], playerWallets);
+  const wallet2 = payOutToWallet(payouts[1], playerWallets);
+
+  const transactionBuildOptions = new TransactionBuildOptions()
+    .withTxIns(utxoStackAsInput(utxoStackForWallet(playerWallet))) //for fees
+    .withTxIn(utxoAsTransactionBuildInput(utxoFromStack, commandRedeemerile, scriptFile)) //winning game input
+    .withRequiredSigner(RequiredSigner.file(playerWallet.keys.payment.signingKeyFile))
+    .withTxInCollateral(utxoStackForWallet(playerWallet).utxos[0].id.toString()) //just take the first- blindly
+    .withTxOut(gamePayoutAsTxOutParameters(payouts[0], wallet1))
+    .withTxOut(gamePayoutAsTxOutParameters(payouts[1], wallet2))
+    .withChangeAddress(playerWallet.paymentAddr);
+
+  //draft transaction
+  const draftTransactionBodyFile = cardanoCli.transactionBuild(transactionBuildOptions);
+  // sign transaction
+  const signedTransactionFile = cardanoCli.transactionSign(
+    new TransactionSignOptions(TxToSign.txBodyFile(draftTransactionBodyFile), [
+      new SigningKeyFile(playerWallet.keys.payment.signingKeyFile),
+    ])
+  );
+
+  //send
+  const txHash = cardanoCli.transactionSubmit(new TransactionSubmitOptions(signedTransactionFile));
+  const wallet1TxUtxoId = new UtxoId(txHash, 0);
+  const wallet2TxUtxoId = new UtxoId(txHash, 1);
+
+  //wait for one of the utxos to arrive
+  cardanoCli.waitForUtxoAtPaymentAddress(wallet1.paymentAddr, wallet1TxUtxoId, 600);
+  return [wallet1TxUtxoId, wallet2TxUtxoId];
+};
