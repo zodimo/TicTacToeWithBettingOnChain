1c064020a8029ad1dd0bf7508220e75bcdaa5039
Jaco Labuschagne <zodimo@gmail.com>
2023-01-04 17:25:20 +0200

    back to real script

--- src/examples/utxo-stack-at-script-address.ts
+++ src/examples/utxo-stack-at-script-address.ts
@@ -1,7 +1,7 @@
 import { cardanoCli } from "../previewCardanoCliJs.js";
 import { getScriptAddress, getTypedAlwaysSucceedScriptAddress, getUntypedAlwaysSucceedScriptAddress } from "../smart-contract.js";
-// const scriptAddress = getScriptAddress();
+const scriptAddress = getScriptAddress();
 // const scriptAddress = getUntypedAlwaysSucceedScriptAddress();
-const scriptAddress = getTypedAlwaysSucceedScriptAddress();
+// const scriptAddress = getTypedAlwaysSucceedScriptAddress();
 console.log(`ScriptAddress: ${scriptAddress}`);
 console.log(JSON.stringify(cardanoCli.getUtxoStackForAddress(scriptAddress),null,2));
