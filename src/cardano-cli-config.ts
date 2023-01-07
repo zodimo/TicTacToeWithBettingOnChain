256764f1f9aecbfd18ecafdea80ed011b6116d37
Jaco Labuschagne <zodimo@gmail.com>
2023-01-07 12:08:20 +0200

    add dotenv

--- src/cardano-cli-config.ts
+++ src/cardano-cli-config.ts
@@ -4,6 +4,8 @@ import { Era } from "./cardano-cli/command/era.js";
 import { Network } from "./cardano-cli/command/network.js";
 import { NodeMode } from "./cardano-cli/command/node-mode.js";
 
+import dotenv from "dotenv";
+
 class Config {
   private cardanoCliBinPath: string;
   private workingDirectory: string;
@@ -12,17 +14,23 @@ class Config {
   private nodeMode: NodeMode;
   private debug: boolean;
 
-  constructor() {
-    this.cardanoCliBinPath = "cardano-cli";
-    this.workingDirectory = path.join(
-      os.homedir(),
-      "CardanoProjects",
-      "testnet"
-    );
-    this.network = Network.testnetMagic(2);
+  constructor(cardanoCliBin?: string, workingDirectory?: string, testnetMagicNumber?: number, debug?: boolean) {
+    this.cardanoCliBinPath = cardanoCliBin ?? "cardano-cli";
+    // Jaco default
+    // this.workingDirectory = workingDirectory ?? path.join(os.homedir(), "CardanoProjects", "testnet");
+    this.workingDirectory = workingDirectory ?? "./workdir";
+    this.network = Network.testnetMagic(testnetMagicNumber ?? 2);
     this.era = Era.babbage();
     this.nodeMode = NodeMode.cardano();
-    this.debug = true;
+    this.debug = debug ?? true;
+  }
+
+  static createFromEnv(): Config {
+    const cardanoCliBinPath = process.env.CARDANO_CLI_BIN_PATH ?? "cardano-cli";
+    const workingDirectory = process.env.WORKING_DIRECTORY;
+    const networkTestnetMagicNumber = process.env.NETWORK_TESTNET_MAGIC_NUMBER ?? 2;
+    const debug = !!process.env.CARDANO_CLI_DEBUG ?? true;
+    return new Config(cardanoCliBinPath, workingDirectory, +networkTestnetMagicNumber, debug);
   }
 
   getCardanoCliBinPath(): string {
@@ -50,4 +58,6 @@ class Config {
 }
 
 //load dot env here..
-export const config = new Config();
+dotenv.config();
+export const config = Config.createFromEnv();
+
